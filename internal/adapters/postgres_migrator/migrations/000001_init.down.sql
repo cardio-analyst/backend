3d9af03a7c47ac9e9f9f09b638b21ae211ff8a58
tmrrwnxtsn <tmrrwnxtsn@gmail.com>
2022-11-06 02:03:30 +0400

    Join migrations

--- internal/adapters/postgres_migrator/migrations/000001_init.down.sql
+++ internal/adapters/postgres_migrator/migrations/000001_init.down.sql
@@ -1,8 +1,25 @@
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS sessions;
+
+DROP TABLE IF EXISTS diseases;
+
+DROP TABLE IF EXISTS analyses;
+
+DROP TABLE IF EXISTS lifestyles;
+
 DROP TABLE IF EXISTS very_high_risk_female_not_smoking;
+
 DROP TABLE IF EXISTS very_high_risk_male_not_smoking;
+
 DROP TABLE IF EXISTS very_high_risk_female_smoking;
+
 DROP TABLE IF EXISTS very_high_risk_male_smoking;
+
 DROP TABLE IF EXISTS high_risk_female_not_smoking;
+
 DROP TABLE IF EXISTS high_risk_male_not_smoking;
+
 DROP TABLE IF EXISTS high_risk_female_smoking;
+
 DROP TABLE IF EXISTS high_risk_male_smoking;
--- internal/adapters/postgres_migrator/migrations/000002_create_sessions_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS sessions
-(
-    user_id       INTEGER PRIMARY KEY,
-    refresh_token VARCHAR(255)   NOT NULL,
-    whitelist     VARCHAR(255)[] NOT NULL,
-    CONSTRAINT fk_sessions_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
