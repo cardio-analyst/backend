9d868f77a0ae3e2d85d442ac4eed0efe78e81ba8
BossMouseFire <abanin-02@mail.ru>
2022-11-27 19:55:28 +0400

    added last recomendations

--- internal/domain/service/recommendations.go
+++ internal/domain/service/recommendations.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"bytes"
+	"fmt"
+	"math"
 	"math/rand"
 	"text/template"
 	"time"
@@ -17,14 +19,22 @@ const templatesPattern = "templates/recommendations/*.tmpl"
 
 // recommendation template names
 const (
-	templateHealthyEating = "healthy_eating.tmpl"
-	templateSmoking       = "smoking.tmpl"
+	templateHealthyEating    = "healthy_eating.tmpl"
+	templateSmoking          = "smoking.tmpl"
+	templateSBPLevel         = "sbp_level.tmpl"
+	templateCholesterolLevel = "cholesterol_level.tmpl"
+	templateBMI              = "bmi.tmpl"
+	templateLifestyle        = "lifestyle.tmpl"
 )
 
 // recommendation titles
 const (
-	titleHealthyEating = "Здоровое питание"
-	titleSmoking       = "Отказ от курения"
+	titleHealthyEating    = "Здоровое питание"
+	titleSmoking          = "Отказ от курения"
+	titleSBPLevel         = "АД"
+	titleCholesterolLevel = "Холестерин"
+	titleBMI              = "Ожирение"
+	titleLifestyle        = "Образ жизни"
 )
 
 // check whether recommendationsService structure implements the service.RecommendationsService interface
@@ -69,7 +79,37 @@ func (s *recommendationsService) GetRecommendations(userID uint64) ([]*models.Re
 	}
 
 	var err error
-	recommendation, err = s.getSmokingRecommendation(userID)
+
+	basicIndicators, err := s.basicIndicators.FindAll(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	recommendation, err = s.getSmokingRecommendation(userID, basicIndicators)
+	if err != nil {
+		return nil, err
+	}
+	if recommendation != nil {
+		recommendations = append(recommendations, recommendation)
+	}
+
+	recommendation, err = s.getSBPLevelRecommendation(basicIndicators[0])
+	if err != nil {
+		return nil, err
+	}
+	if recommendation != nil {
+		recommendations = append(recommendations, recommendation)
+	}
+
+	recommendation, err = s.getBMIRecommendation(basicIndicators)
+	if err != nil {
+		return nil, err
+	}
+	if recommendation != nil {
+		recommendations = append(recommendations, recommendation)
+	}
+
+	recommendation, err = s.getLifestyleRecommendation()
 	if err != nil {
 		return nil, err
 	}
@@ -97,39 +137,16 @@ func (s *recommendationsService) getHealthyEatingRecommendation() *models.Recomm
 	}
 }
 
-func (s *recommendationsService) getSmokingRecommendation(userID uint64) (*models.Recommendation, error) {
-	basicIndicators, err := s.basicIndicators.FindAll(userID)
-	if err != nil {
-		return nil, err
-	}
+func (s *recommendationsService) getSmokingRecommendation(userID uint64, basicIndicators []*models.BasicIndicators) (*models.Recommendation, error) {
 
-	var scoreData models.ScoreData
-	for _, basicIndicator := range basicIndicators {
-		if basicIndicator.Smoking != nil && *basicIndicator.Smoking {
-			scoreData.Smoking = true
-		}
-		if basicIndicator.Gender != nil && scoreData.Gender == "" {
-			scoreData.Gender = *basicIndicator.Gender
-		}
-		if basicIndicator.SBPLevel != nil && scoreData.SBPLevel == 0 {
-			scoreData.SBPLevel = *basicIndicator.SBPLevel
-		}
-		if basicIndicator.TotalCholesterolLevel != nil && scoreData.TotalCholesterolLevel == 0 {
-			scoreData.TotalCholesterolLevel = *basicIndicator.TotalCholesterolLevel
-		}
-
-		// fastest break condition
-		if scoreData.Gender != "" && scoreData.SBPLevel != 0 && scoreData.TotalCholesterolLevel != 0 {
-			break
-		}
-	}
+	scoreData := getScoreData(basicIndicators)
 
 	if !scoreData.Smoking {
 		return nil, nil
 	}
 
 	var user *models.User
-	user, err = s.users.GetByCriteria(models.UserCriteria{
+	user, err := s.users.GetByCriteria(models.UserCriteria{
 		ID: &userID,
 	})
 	if err != nil {
@@ -164,4 +181,135 @@ func (s *recommendationsService) getSmokingRecommendation(userID uint64) (*model
 		Title:       titleSmoking,
 		Description: tmplBuffer.String(),
 	}, nil
+}
+
+func (s *recommendationsService) getSBPLevelRecommendation(basicIndicator *models.BasicIndicators) (*models.Recommendation, error) {
+	sbpLevel := *basicIndicator.SBPLevel
+
+	if sbpLevel >= 140 {
+		tmplBuffer := &bytes.Buffer{}
+		if err := s.templates.ExecuteTemplate(tmplBuffer, templateSBPLevel, map[string]interface{}{}); err != nil {
+			return nil, err
+		}
+
+		return &models.Recommendation{
+			Title:       titleSBPLevel,
+			Description: tmplBuffer.String(),
+		}, nil
+	}
+
+	return nil, nil
+}
+
+func (s *recommendationsService) getBMIRecommendation(basicIndicators []*models.BasicIndicators) (*models.Recommendation, error) {
+	bmi := math.Pow(*basicIndicators[0].Weight/(*basicIndicators[0].Height/100), 2)
+	scoreData := getScoreData(basicIndicators)
+
+	var waist string
+
+	if scoreData.Gender == "male" && bmi > 102 {
+		waist = "Также у Вас превышен объем талии, необходимо уменьшить его минимум до 102"
+	}
+
+	if scoreData.Gender == "female" && bmi > 88 {
+		waist = "Также у Вас превышен объем талии, необходимо уменьшить его минимум до 88"
+	}
+
+	tmplBuffer := &bytes.Buffer{}
+	if err := s.templates.ExecuteTemplate(tmplBuffer, templateBMI, map[string]interface{}{
+		"bmi":   fmt.Sprintf("%.2f", bmi),
+		"waist": waist,
+	}); err != nil {
+		return nil, err
+	}
+
+	return &models.Recommendation{
+		Title:       titleBMI,
+		Description: tmplBuffer.String(),
+	}, nil
+}
+
+func (s *recommendationsService) getLifestyleRecommendation() (*models.Recommendation, error) {
+	tmplBuffer := &bytes.Buffer{}
+	if err := s.templates.ExecuteTemplate(tmplBuffer, templateLifestyle, map[string]interface{}{}); err != nil {
+		return nil, err
+	}
+
+	return &models.Recommendation{
+		Title:       titleLifestyle,
+		Description: tmplBuffer.String(),
+	}, nil
+}
+
+func (s *recommendationsService) getCholesterolLevelRecommendation(basicIndicators []*models.BasicIndicators, userID uint64) (*models.Recommendation, error) {
+	scoreData := getScoreData(basicIndicators)
+
+	totalCholesterolLevel := scoreData.TotalCholesterolLevel
+	gender := scoreData.Gender
+
+	userDiseases, err := s.diseases.Get(userID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	statusCholesterol := totalCholesterolLevel > 5 || (totalCholesterolLevel > 4.5 && userDiseases.HasTypeTwoDiabetes)
+
+	if !statusCholesterol {
+		return nil, nil
+	}
+
+	var minCholesterol string
+
+	if userDiseases.HasTypeTwoDiabetes {
+		minCholesterol = "4-4.5"
+	} else {
+		minCholesterol = "5"
+	}
+
+	var maxAlcohol string
+
+	if gender == "female" {
+		maxAlcohol = "10-20"
+	} else {
+		maxAlcohol = "20-30"
+	}
+
+	tmplBuffer := &bytes.Buffer{}
+	if err = s.templates.ExecuteTemplate(tmplBuffer, templateCholesterolLevel, map[string]interface{}{
+		"minCholesterol": minCholesterol,
+		"maxAlcohol":     maxAlcohol,
+	}); err != nil {
+		return nil, err
+	}
+
+	return &models.Recommendation{
+		Title:       titleCholesterolLevel,
+		Description: tmplBuffer.String(),
+	}, nil
+}
+
+func getScoreData(basicIndicators []*models.BasicIndicators) models.ScoreData {
+	var scoreData models.ScoreData
+	for _, basicIndicator := range basicIndicators {
+		if basicIndicator.Smoking != nil && *basicIndicator.Smoking {
+			scoreData.Smoking = true
+		}
+		if basicIndicator.Gender != nil && scoreData.Gender == "" {
+			scoreData.Gender = *basicIndicator.Gender
+		}
+		if basicIndicator.SBPLevel != nil && scoreData.SBPLevel == 0 {
+			scoreData.SBPLevel = *basicIndicator.SBPLevel
+		}
+		if basicIndicator.TotalCholesterolLevel != nil && scoreData.TotalCholesterolLevel == 0 {
+			scoreData.TotalCholesterolLevel = *basicIndicator.TotalCholesterolLevel
+		}
+
+		// fastest break condition
+		if scoreData.Gender != "" && scoreData.SBPLevel != 0 && scoreData.TotalCholesterolLevel != 0 {
+			break
+		}
+	}
+
+	return scoreData
 }
