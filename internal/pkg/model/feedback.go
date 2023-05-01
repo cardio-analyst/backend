deaae8b16fcfbab7b10e4efd04f7cc1468dd7041
tmrrwnxtsn <tmrrwnxtsn@gmail.com>
2023-05-02 01:21:55 +0400

    Change feedback param names

--- internal/pkg/model/feedback.go
+++ internal/pkg/model/feedback.go
@@ -2,12 +2,12 @@ package model
 
 type Feedback struct {
 	ID             uint64   `json:"-" db:"id"`
-	UserID         uint64   `json:"user_id" db:"user_id"`
-	UserFirstName  string   `json:"user_first_name" db:"user_first_name"`
-	UserLastName   string   `json:"user_last_name" db:"user_last_name"`
-	UserMiddleName string   `json:"user_middle_name,omitempty" db:"user_middle_name,omitempty"`
-	UserLogin      string   `json:"user_login" db:"user_login"`
-	UserEmail      string   `json:"user_email" db:"user_email"`
+	UserID         uint64   `json:"userId" db:"user_id"`
+	UserFirstName  string   `json:"userFirstName" db:"user_first_name"`
+	UserLastName   string   `json:"userLastName" db:"user_last_name"`
+	UserMiddleName string   `json:"userMiddleName,omitempty" db:"user_middle_name,omitempty"`
+	UserLogin      string   `json:"userLogin" db:"user_login"`
+	UserEmail      string   `json:"userEmail" db:"user_email"`
 	Mark           int16    `json:"mark" db:"mark"`
 	Message        string   `json:"message,omitempty" db:"message,omitempty"`
 	CreatedAt      Datetime `json:"createdAt" db:"created_at"`
